61158066e877cde60e936b709a6d6133983de51f
r-lyeh <rlyeh.nospam@gmail.com>
2015-04-21 13:27:31 +0200

    Update sample.cc

--- sample.cc
+++ sample.cc
@@ -2,14 +2,15 @@
 #include <vector_view>
 
 int main() {
-   std::vector<int> abc = {1,2,3};
-   std::vector_view<int> abc2 = abc;
-   std::vector_view<int> abc3( abc.data(), abc.size() );
+   int mem[] = {1,2,3};
+   std::vector<int> abc1 = { 1,2,3 };
+   std::vector_view<int> abc2 = abc1;
+   std::vector_view<int> abc3( abc1.data(), abc1.size() );
+   std::vector_view<int> abc4( &mem[0], 3 );
 
-   for( auto &i : abc2 ) {
-      std::cout << i << std::endl;
-   }
-   for( auto &i : abc3 ) {
-      std::cout << i << std::endl;
-   }
+   for( auto &i : mem  ) std::cout << i << ','; std::cout << std::endl;
+   for( auto &i : abc1 ) std::cout << i << ','; std::cout << std::endl;
+   for( auto &i : abc2 ) std::cout << i << ','; std::cout << std::endl;
+   for( auto &i : abc3 ) std::cout << i << ','; std::cout << std::endl;
+   for( auto &i : abc4 ) std::cout << i << ','; std::cout << std::endl;
 }
